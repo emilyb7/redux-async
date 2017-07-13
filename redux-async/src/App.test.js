c55d2dbd0905c8d205819f9b7636241ebd645761
emily <limeyb7@gmail.com>
2017-07-13 16:14:02 +0100

    fixes broken tests

--- redux-async/src/App.test.js
+++ redux-async/src/App.test.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/* redux stuff */
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer.js';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={ createStore(reducer) }>
+      <App />
+    </Provider>, div
+  );
 });
